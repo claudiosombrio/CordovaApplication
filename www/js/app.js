d76d4d4039ae2d2e5b8c71a7d8f6e2e0a9203689
claudio <claudiosombrio@gmail.com>
2014-07-11 17:32:24 -0300

    plugin

--- www/js/app.js
+++ www/js/app.js
@@ -2,17 +2,6 @@
 (function() {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
-    var service = new EmployeeService();
-    service.initialize().done(function() {
-        console.log("Service initialized");
-    });
-
-    /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Employee Directory v3.4");
-    });
-
     document.addEventListener('deviceready', function() {
         if (navigator.notification) { // Override default HTML alert with native dialog
             window.alert = function(message) {
@@ -24,20 +13,33 @@
                         );
             };
         }
-    }, false);
 
-
-    /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+        var service = new EmployeeService();
+        service.initialize().done(function() {
+            console.log("Service initialized");
         });
-    }
+
+        /* --------------------------------- Event Registration -------------------------------- */
+        $('.search-key').on('keyup', findByName);
+        $('.help-btn').on('click', function() {
+            alert("Employee Directory v3.4");
+        });
+
+
+        /* ---------------------------------- Local Functions ---------------------------------- */
+        function findByName() {
+            service.findByName($('.search-key').val()).done(function(employees) {
+                var l = employees.length;
+                var e;
+                $('.employee-list').empty();
+                for (var i = 0; i < l; i++) {
+                    e = employees[i];
+                    $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+                }
+            });
+        }
+
+
+    }, false);
 
 }());
