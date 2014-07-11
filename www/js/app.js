67b6def6f8489b6935f7e8587849b2b16dfd69b8
claudio <claudiosombrio@gmail.com>
2014-07-11 16:12:58 -0300

    plugin

--- www/js/app.js
+++ www/js/app.js
@@ -1,31 +1,36 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
-(function () {
-
+(function() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    
+function onDeviceReady() {
     /* ---------------------------------- Local Variables ---------------------------------- */
+    alert("Application ready");
     var service = new EmployeeService();
-    service.initialize().done(function () {
+    service.initialize().done(function() {
         console.log("Service initialized");
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
     $('.search-key').on('keyup', findByName);
     $('.help-btn').on('click', function() {
-        
-        
+
+
         alert("Employee Directory v3.4");
     });
+}
 
-    /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    }
-
+/* ---------------------------------- Local Functions ---------------------------------- */
+function findByName() {
+    service.findByName($('.search-key').val()).done(function(employees) {
+        var l = employees.length;
+        var e;
+        $('.employee-list').empty();
+        for (var i = 0; i < l; i++) {
+            e = employees[i];
+            $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+        }
+    });
+}
+    
 }());
+
