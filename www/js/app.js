e324eb28bcd75d0c23aca20b37c13d40f6e88552
claudio <claudiosombrio@gmail.com>
2014-07-14 10:28:23 -0300

    home dynamic

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,21 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function() {
 
-    /* ---------------------------------- Local Variables ---------------------------------- */
-    document.addEventListener('deviceready', function() {
+    document.addEventListener('deviceready', ready(),false);
+
+    function ready(){
+        dialogsModifier();
+        dbInitializer();
+        eventRegistration();
+    }    
+    
+    function eventRegistration(){
+        $('.help-btn').on('click', function() {
+            alert("Employee Directory v3.4");
+        });
+    }
+    
+    function dialogsModifier(){
         if (navigator.notification) { // Override default HTML alert with native dialog
             window.alert = function(message) {
                 navigator.notification.alert(
@@ -13,34 +26,38 @@
                         );
             };
         }
-
+    }
+    
+    function dbInitializer(){
         var service = new EmployeeService();
         service.initialize().done(function() {
             console.log("Service initialized");
+            renderHomeView();
         });
+    }
 
-        /* --------------------------------- Event Registration -------------------------------- */
+    function renderHomeView(){
+        var html = "<div class='header'><h1>Directory</h1></div>" +
+            "<div class='search-view'>" +
+                "<input class='search-key' type='search' placeholder='Enter name'/>" +
+                "<ul class='list employee-list'></ul>" +
+            "</div>";
+        $('body').html(html);
+
         $('.search-key').on('keyup', findByName);
-        $('.help-btn').on('click', function() {
-            alert("Employee Directory v3.4");
-        });
+    }
 
-
-        /* ---------------------------------- Local Functions ---------------------------------- */
-        function findByName() {
-            service.findByName($('.search-key').val()).done(function(employees) {
-                var l = employees.length;
-                var e;
-                $('.employee-list').empty();
-                for (var i = 0; i < l; i++) {
-                    e = employees[i];
+    function findByName() {
+        service.findByName($('.search-key').val()).done(function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i = 0; i < l; i++) {
+                e = employees[i];
 //                    $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-                    $('.employee-list').append('<li><a href="#employees/' + e.sqlite_version + '">' + e.sqlite_version + '</a></li>');
-                }
-            });
-        }
-
-
-    }, false);
+                $('.employee-list').append('<li><a href="#employees/' + e.sqlite_version + '">' + e.sqlite_version + '</a></li>');
+            }
+        });
+    }
 
 }());
